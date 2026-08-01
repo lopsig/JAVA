89234dfcf1fdf0ce49d817d60c8e8762385f7eea
lopsig <jonathan.ls.trabajo@gmail.com>
2026-07-31 20:00:34 -0500

    feat: add Mensajeria and TestMensajeria classes

---
+++ src/com/krakedev/TestMensajeria.java
@@ -0,0 +1,11 @@
+package com.krakedev;
+
+public class TestMensajeria {
+    public static void main(String[] args) {
+        Mensajeria m1 =  new Mensajeria();
+        m1.saludar();
+
+        m1.saludarPersona("Jonathan", "LS");
+
+    }
+}

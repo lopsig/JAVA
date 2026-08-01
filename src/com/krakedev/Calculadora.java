41d8bfaa65b9b6b1a1cfed988084b3dcf6be4e46
lopsig <jonathan.ls.trabajo@gmail.com>
2026-07-31 21:27:10 -0500

    feat: add methods to Calculadora class and test cases in TestCalculadora

--- src/com/krakedev/Calculadora.java
+++ src/com/krakedev/Calculadora.java
@@ -1,15 +1,51 @@
 package com.krakedev;
 
 public class Calculadora {
+    //FUNCION SUMAR
     public int sumar (int a, int b){
         int resultado;
         resultado = a + b;
         return resultado;
     }
 
+    //FUNCION RESTAR
     public int restar (int a, int b){
         int resultado;
         resultado = a - b;
         return resultado;
     }
+
+    //FUNCION MULTIPLICAR
+    public double multiplicar (double valor1, double valor2){
+        double resultado;
+        resultado = valor1 * valor2;
+        return resultado;
+    }
+
+    //FUNCION DIVIDIR
+    public double dividir (double dividendo, double divisor){
+        double cociente;
+        cociente = dividendo / divisor;
+
+        if (divisor == 0){
+            System.out.println("No se puede dividir para cero");
+        }
+
+        return cociente;
+    }
+
+    //FUNCION PROMEDIAR
+    public double promediar (double valor1, double valor2, double valor3){
+        double promedio;
+        promedio = (valor1 + valor2 + valor3)/3;
+        return promedio;
+    }
+
+    // FUNCION RESULTADO
+    public void mostrarResultado () {
+        System.out.println("Ahorita no joven, salí al almuerzo. Regreso en 15 minutos");
+    }
+
+
+
 }
--- src/com/krakedev/TestCalculadora.java
+++ src/com/krakedev/TestCalculadora.java
@@ -2,15 +2,41 @@ package com.krakedev;
 
 public class TestCalculadora {
     public static void main(String[] args) {
+        // INSTANCIA
         Calculadora c1 = new Calculadora();
 
+        //VARIABLES
         int r1;
+        int r2;
+        double r3;
+        double r4;
+        double r5;
+
+
+        //INVOCACION DE METODOS
+        // METOD SUMAR
         r1 = c1.sumar(3,7);
         System.out.println("Suma: " + r1);
 
-        int r2;
+        // METODO RESTAR
         r2 = c1.restar(7,3);
         System.out.println("Resta: " + r2);
+
+        //METODO MULTIPLICAR
+        r3 = c1.multiplicar(7, 3);
+        System.out.println("Multiplicacion: " + r3);
+
+        //METODO DIVIDIR
+        r4 = c1.dividir(7, 0);
+        System.out.println("Dividisión: " + r4);
+
+        //METODO PROMEDIAR
+        r5 = c1.promediar(7, 3, 12);
+        System.out.println("Promedio: " + r5);
+
+        //METODO RESULTADO
+        c1.mostrarResultado();
+
 
     }
 }
